c2d4a73595a31cc86bbfc59fde130e94ec11f316
david-uldrich <54560966+david-uldrich@users.noreply.github.com>
2019-09-16 17:45:13 -0400

    Add files via upload

---
+++ Exercise 2-2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+# Connect to sqlite db
+db_file = r'datasets/salesdata.db'
+engine = create_engine(r"sqlite:///{}"
+                       .format(db_file))
+sql = "SELECT * from scores"
+sales_data_df = pd.read_sql(sql, engine)
+sales_data_df
+
